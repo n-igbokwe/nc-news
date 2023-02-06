ddd9bad24c1e57fe6c95b8d0389c6f003be19577
n-igbokwe <nathanigbokwe@hotmail.com>
2023-02-06 21:57:56 +0000

    ticket 5

--- src/components/singleArticle.jsx
+++ src/components/singleArticle.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
-
+import { useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import { getSingleArticle } from '../utils/api'
+import {Link} from 'react-router-dom'
 function SingleArticle() {
+    const [singleArticle, setSingleArticle] = useState([])
+    const {article_id} = useParams()
+    useEffect(() => {
+        getSingleArticle(article_id)
+        .then(({data : {article}}) => {
+            setSingleArticle(article)
+        })
+    })
   return (
-    <div>singleArticle</div>
+    <section>
+        {singleArticle.length !== 0 ? (
+            singleArticle.map((article) =>
+            <>
+            <h2 key={article.article_id}>{article.title}</h2>
+            <Link to={`/articles/${article.topic}`}>{article.topic}
+            </Link>
+            <br></br>
+            <br></br>
+            <Link to={`/articles/${article.author}`}>
+                {article.author}
+            </Link>
+            <br></br>
+            <br></br>
+            <img src={article.article_img_url} alt={article.title}></img>
+            <p>{article.body}</p>
+            <p> Published : {article.created_at}</p>
+            <p>Votes :{article.votes} | Comments: {article.comment_count} </p>
+            </>
+            )) : (<p> Loading</p>)
+        }
+    <h2></h2>
+    </section>
   )
 }
-
 export default SingleArticle
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,23 @@
 import axios from "axios";
-
 const articleAPI = axios.create({
     baseURL: 'https://be-news-api.onrender.com/api/'
 })
-
+//ticket 4
 export const getAllArticles = () => {
     return articleAPI
     .get('/articles')
     .then((data) => {
         return data
+    })
+}
+//ticket5
+export const getSingleArticle = (article) => {
+    return articleAPI
+    .get(`/articles/${article}`)
+    .then((data) => {
+        return data
+    })
+    .catch((err)=> {
+        console.log(err)
     })
 }
